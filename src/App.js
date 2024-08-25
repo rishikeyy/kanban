f23c042270b4424e4c9953ed2f25980cab7c1bf2
_bidhayak <rishikeyyadav@gmail.com>
2024-08-25 22:43:23 +0530

    "first addTask"

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+    <addTask/>
+    <zones/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/addTask.js
@@ -0,0 +1,18 @@
+import react from "react";
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+function addTask(){
+
+    return (
+        <>
+        <div>
+           <textarea name="add task" placeholder="Add task" /> 
+           <Fab color="primary" aria-label="add">
+                <AddIcon />
+          </Fab>
+        </div>
+        </>
+    )
+}
+
+export default addTask;
